24de16e370ce8574cdb4c1d21658271e82bc0bf0
David Verbeck <50149500+davidverb@users.noreply.github.com>
2020-05-10 17:02:09 -0600

    Inital Upload
    Inital upload of a Survial Shooter Unity Game

---
+++ Assets/Scripts/Player/GetLineRendererWidth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetLineRendererWidth : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        float startWidth = lineRenderer.startWidth;
+        float endWidth = lineRenderer.endWidth;
+        Debug.Log("Start width: " + startWidth + '\t' + "End width: " + endWidth);
+    }
+}
